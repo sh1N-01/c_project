9016adf1db215eabbc7e79162f747d9e8a6559ac
sh1N-01 <paranaque.renz02@gmail.com>
2024-10-01 20:14:43 +0800

    Pluck

--- Code for fun/fibonaccii.c
+++ Code for fun/fibonaccii.c
@@ -7,24 +7,24 @@ int main()
 {
     fflush(stdout);
 
-    SetConsoleTitle("Fibonnaci Sequence");
+    SetConsoleTitle("Fibonacci Sequence");
     //Ask user input
-    unsigned long long fibonnaci, fibo1, fibo0;
+    unsigned long long fibonacci, fibo1, fibo0;
     int num; 
-    fibonnaci = 0;
+    fibonacci = 0;
     fibo1 = 0;
     fibo0 = 1;
-    printf("Enter the desired nth term for Fibonnaci: ");
+    printf("Enter the desired nth term for Fibonacci: ");
     scanf("%d", &num);
     //Fibonnaci computation
     for(int i = 1; i <= num; i++)
     {
         //Updating output
-        printf("%d.) %llu\n", i, fibonnaci);
+        printf("%d.) %llu\n", i, fibonacci);
 
-        fibonnaci = fibo1 + fibo0;
+        fibonacci = fibo1 + fibo0;
         fibo0 = fibo1;
-        fibo1 = fibonnaci;
+        fibo1 = fibonacci;
     }
 
     sleep(5);
